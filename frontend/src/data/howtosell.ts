16d48956abbd194c7f9f7337af1963d62500e792
dgvdilshan@students.nsbm.ac.lk <vimukthidilshan23933@gmail.com>
2024-09-27 13:03:11 +0530

    create selling part

---
+++ frontend/src/data/howtosell.ts
@@ -0,0 +1,32 @@
+export const howToSell = [
+  {
+    stepNo: 1,
+    stepType: 'Registration',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '/signup',
+  },
+  {
+    stepNo: 2,
+    stepType: 'Select Platform',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '',
+  },
+  {
+    stepNo: 3,
+    stepType: 'Listing the Item',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '',
+  },
+  {
+    stepNo: 4,
+    stepType: 'Managing the Auction',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '',
+  },
+  {
+    stepNo: 5,
+    stepType: 'Closing the Auction',
+    desc: 'Clearly state your pricing structure, payment terms and and any additional charges. Specifying when & payment invoices will be issued, as well as your accepted.',
+    link: '',
+  },
+];
